4e901b9dc9b52b4fb83858155b4a144aa1578eb3
firth-nodot2 <firthnodot2@gmail.com>
2025-06-20 17:26:55 +0800

    First Commit

---
+++ src/main/java/org/rhydo/superecom/SuperEcomApplication.java
@@ -0,0 +1,13 @@
+package org.rhydo.superecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperEcomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperEcomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rhydo/superecom/SuperEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rhydo.superecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperEcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
