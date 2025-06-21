bedcadd4f2f44cf4b2aa0511fd04a041eed048d9
firth-nodot2 <firthnodot2@gmail.com>
2025-06-21 14:45:10 +0800

    Make category persistent

--- src/main/java/org/rhydo/superecom/service/CategoryServiceImpl.java
+++ src/main/java/org/rhydo/superecom/service/CategoryServiceImpl.java
@@ -1,43 +1,47 @@
 package org.rhydo.superecom.service;
 
 import org.rhydo.superecom.model.Category;
+import org.rhydo.superecom.repository.CategoryRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
-    private List<Category> categories =  new ArrayList<>();
-    private Long nextId = 1L;
+    private final CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
-        categories.add(category);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
+        List<Category> categories = categoryRepository.findAll();
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "resource not found"));
 
-        categories.remove(category);
+        categoryRepository.delete(category);
         return "Category with categoryId " + category.getCategoryId() + " has been deleted";
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
+        List<Category> categories = categoryRepository.findAll();
         Optional<Category> optionalCategory = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst();
@@ -45,7 +49,7 @@ public class CategoryServiceImpl implements CategoryService {
         if (optionalCategory.isPresent()) {
             Category existingCategory = optionalCategory.get();
             existingCategory.setCategoryName(category.getCategoryName());
-            return existingCategory;
+            return categoryRepository.save(existingCategory);
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
         }
