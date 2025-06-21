ffc29d9e85882ab8e47383b7ffcb939ba1ca4c37
firth-nodot2 <firthnodot2@gmail.com>
2025-06-21 14:33:07 +0800

    Setup CategoryRepository

---
+++ src/main/java/org/rhydo/superecom/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package org.rhydo.superecom.repository;
+
+import org.rhydo.superecom.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
