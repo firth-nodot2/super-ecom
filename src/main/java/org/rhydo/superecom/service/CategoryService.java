0960a8ef7e441b51abb4d83e9c097da5a09dd827
firth-nodot2 <firthnodot2@gmail.com>
2025-06-21 09:08:53 +0800

    Setup Category Service

---
+++ src/main/java/org/rhydo/superecom/service/CategoryService.java
@@ -0,0 +1,11 @@
+package org.rhydo.superecom.service;
+
+import org.rhydo.superecom.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+
+    void createCategory(Category category);
+}
